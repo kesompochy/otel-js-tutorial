aa125db0bce895d49300ec7eee19553166263240
kesompochy <kesompochy@gmail.com>
2024-06-02 02:13:56 +0900

    backend

---
+++ backend/src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from "@nestjs/common";
+import { InjectConnection } from "@nestjs/typeorm";
+import { Connection } from "typeorm";
+
+@Controller()
+export class AppController {
+	constructor(
+          @InjectConnection() private connection: Connection
+        ) {}
+  
+        async getData(): Promise<string> {
+          const result = await this.connection.query('SELECT NOW()');
+          return `Current date and time is ${result[0].now}`;
+        }
+}
