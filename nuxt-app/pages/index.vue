a141ac11c5ebb0d0eec41ba64523e7731fee94ce
kesompochy <kesompochy@gmail.com>
2024-06-02 02:04:37 +0900

    frontend

---
+++ nuxt-app/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Frontend Server</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello, Nuxt!'
+    }
+  }
+}
+</script>
